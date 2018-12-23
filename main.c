fc8cb89210e91734cd2b9b5cb79290f9d94bebc8
Oded Arlevski <arlevski@gmail.com>
2018-12-23 18:18:19 +0200

    code cleaning
    code visual cleaning

--- main.c
+++ main.c
@@ -41,8 +41,8 @@ User *AddUser(User *previous) {
     fgets( input, 15, stdin);
 
     User *newUser = malloc(sizeof(User));
-    sscanf(input, "%s %s", newUser->name, &newUser->code);
-    printf("Added:%s Code:%s\n\n",newUser->name, &newUser->code);
+    sscanf(input, "%s %s", newUser->name, newUser->code);
+    printf("Added:%s Code:%s\n\n",newUser->name, newUser->code);
     newUser->next = NULL;
     if(previous != NULL) {
         previous->next = newUser;
@@ -54,59 +54,13 @@ void userCreate();
 
 void readAccess();
 
+
+
+
+
+
+
 int main() {
-
-
-    //readAccess("access.txt");
-
-
-
-
-    int chosenAction;
-    int menuFlag = 1;
-
-
-
-//    do {
-//        printf("Select Action:\n");
-//
-//        scanf("%d", &chosenAction);
-//
-//        switch (chosenAction) {
-//
-//            case 1:
-//                printf("Adding a new user: \n");
-//                userCreate();
-//                break;
-//            case 2:
-//                printf("case 2 \n");
-//                break;
-//            case 3:
-//                printf("case 3 \n");
-//                break;
-//            case 4:
-//                printf("case 4 \n");
-//                break;
-//            case 5:
-//                printf("case 5 \n");
-//                break;
-//            case 6:
-//                printf("case 6 \n");
-////            printf("Search:");
-////            char strUser[];
-////            scanf("%s", &strUser)
-////            search_user(strUser);
-//                break;
-//            case 7:
-//                printf("Thank you and goodbye");
-//                menuFlag = 0;
-//                break;
-//            default:
-//                printf("Action Not Valid! \n");
-//                break;
-//        }
-//    } while(menuFlag!=0);
-
 
     char command[16];
     char input[16];
@@ -135,56 +89,9 @@ int main() {
         }
     }
 
-    //CleanUp(start);
-
     return 0;
 
 }
 
-//void userCreate(){
-//
-//
-//    printf("Enter user information:\n");
-//
-//    printf("Enter  name (20 Characters): ");
-//    scanf("%s", user.name);
-//
-//    printf("Enter access code (8 digits): ");
-//    scanf("%s", user.code);
-//
-//    printf("Enter permission status: ");
-//    scanf("%d", &user.permissionStatus);
-//
-//
-//    printf("Displaying Information:\n");
-//
-//    printf("Name:%s\n",user.name);
-//
-//
-//    printf("access code: %s\n",user.code);
-//
-//    printf("permission status: %d\n", user.permissionStatus);
-//
-//
-//
-//    FILE * fp;
-//    /* open the file for writing*/
-//    fp = fopen ("test.txt","w");
-//
-//    fprintf (fp, "%-20s %-8s %-1d\n", user.name, user.code, user.permissionStatus);
-//
-//
-//
-//
-//
-//
-////    /* write 10 lines of text into the file stream*/
-////    for(i = 0; i < 10;i++){
-////        fprintf (fp, " is line %d\n",i + 1);
-////    }
-//
-//    /* close the file*/
-//    fclose (fp);
-//
-//}
+
 

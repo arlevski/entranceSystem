1506892b0f4a07d79a397c52e5b1f60c802b7414
Oded Arlevski <arlevski@gmail.com>
2018-12-23 09:07:03 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/entranceSystem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/entranceSystem.dir/main.c.o"
+  "entranceSystem.pdb"
+  "entranceSystem"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/entranceSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
